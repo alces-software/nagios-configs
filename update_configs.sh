f48cf7fa6cbe0295da46d6bbb318959148cb7f69
Ross Rodwell <ross.rodwell@alces-software.com>
2018-09-06 12:41:31 +0100

    Added helper script to bulk update config files and updated config files on master

---
+++ update_configs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# Script to bulk update config files
+#
+
+if [ -z ${1} ] || [ -z ${2} ]; then
+    echo "Error! Usage: ${0} <DIRECTIVE> <new value>"
+    echo "e.g. ${0} TOKEN ZSDJzxd2131Xao1"
+    exit 1
+fi
+
+directive=$1
+echo "Directive is: ${directive}"
+
+new_value=$2
+echo "new_value is: ${new_value}"
+
+for config in `ls -1 *.nrds.cfg*`; do
+    
+    echo ""
+    echo "Updating: ${config}"
+    echo ""
+
+    #grep -i "^${directive}" ${config} | sed -e "s|\(\"\).*\(\"\)|\1${new_value}\2|"
+    sed -i "s|\(${directive}=\"\).*\(\"\)|\1${new_value}\2|" ${config}
+
+    echo ""
+    echo ""
+done
+
+rm -f *.cfge
+rc=$?
+if [ ${rc} -ne 0 ]; then
+    echo "Error removing the .cfge backup files made by sed."
+    exit 1
+fi
+
+exit 0
