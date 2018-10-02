604e449efc6312c6d5e04e43cff9919079a56909
Ross Rodwell <ross.rodwell@alces-software.com>
2018-10-02 15:51:23 +0100

    Added check for check_uptime.sh on all config files

---
+++ add_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z $1 ] || [ -z $2 ]; then
+    echo "Error! Usage: $0 <Plugin filename> <Plugin Arguments> <service_description>"
+    echo "e.g. $0 check_uptime.sh 30 Check Uptime"
+    exit 1
+fi
+
+parent_dir="/opt/nagios/nagios-plugins"
+check_name=$1
+check_arguments=$2
+service_description=$3
+
+echo "Adding: ${parent_dir}/${check_name} to ALL configs"
+
+for config in `ls -1 *.nrds.cfg*`; do
+    echo "command[${service_description}]=${parent_dir}/${check_name} ${check_arguments}" >> ${config}
+done
+
+exit 0
