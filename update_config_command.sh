2907b9e6af96437f6e33f38874383c87d1e9d997
Ross Rodwell <ross.rodwell@alces-software.com>
2018-10-02 13:14:30 +0100

    Updated Plugin locations in config files

--- update_config_command.sh
+++ update_config_command.sh
@@ -5,16 +5,16 @@
 #
 
 if [ -z ${1} ] || [ -z ${2} ]; then
-    echo "Error! Usage: ${0} <DIRECTIVE> <new value>"
-    echo "e.g. ${0} TOKEN ZSDJzxd2131Xao1"
+    echo "Error! Usage: ${0} <Current Path or Parent-Path> <New Path>"
+    echo "e.g. ${0} /usr/local/nagios/libexec /opt/nagios/nagios-plugins"
     exit 1
 fi
 
-directive=$1
-echo "Directive is: ${directive}"
+old_path=$1
+echo "old_path is: ${old_path}"
 
-new_value=$2
-echo "new_value is: ${new_value}"
+new_path=$2
+echo "new_path is: ${new_path}"
 
 for config in `ls -1 *.nrds.cfg*`; do
     
@@ -22,7 +22,7 @@ for config in `ls -1 *.nrds.cfg*`; do
     echo "Updating: ${config}"
     echo ""
 
-    sed -i "s|\(${directive}=\"\).*\(\"\)|\1${new_value}\2|" ${config}
+    sed -i "s|${old_path}|${new_path}|" ${config}
 
     echo ""
     echo ""
